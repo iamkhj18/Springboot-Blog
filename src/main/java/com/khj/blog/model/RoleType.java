2be3fa42b2a24d4e5d2f2d839246dcb0eacabf57
iamkhj18 <iamkhj18@naver.com>
2021-05-05 23:35:01 +0900

    view 작업

---
+++ src/main/java/com/khj/blog/model/RoleType.java
@@ -0,0 +1,5 @@
+package com.khj.blog.model;
+
+public enum RoleType {
+	USER, ADMIN
+}
--- src/main/java/com/khj/blog/model/User.java
+++ src/main/java/com/khj/blog/model/User.java
@@ -4,11 +4,12 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.AllArgsConstructor;
@@ -28,7 +29,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
 	private int id;
 
-	@Column(nullable = false, length = 30)
+	@Column(nullable = false, length = 30, unique = true)
 	private String username;
 
 	@Column(nullable = false, length = 100)
@@ -37,8 +38,9 @@ public class User {
 	@Column(nullable = false, length = 50)
 	private String email;
 
-	@ColumnDefault("'user'")
-	private String role; // Enum을 쓰는게 좋다. // admin, user, manager
+	// DB는 RoleType이라는게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role; // Enum을 쓰는게 좋다. // ADMIN, USER
 
 	@CreationTimestamp // 시간 자동 입력
 	private Timestamp createDate;
