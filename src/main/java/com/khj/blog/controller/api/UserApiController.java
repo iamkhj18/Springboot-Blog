1605ee197ff43fb84970e3f4c70e9e698f2c250a
iamkhj18 <iamkhj18@naver.com>
2021-05-08 17:37:30 +0900

    비밀번호 암호화

--- src/main/java/com/khj/blog/controller/api/UserApiController.java
+++ src/main/java/com/khj/blog/controller/api/UserApiController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.khj.blog.dto.ResponseDto;
-import com.khj.blog.model.RoleType;
 import com.khj.blog.model.User;
 import com.khj.blog.service.UserService;
 
@@ -20,7 +19,6 @@ public class UserApiController {
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨");
-		user.setRole(RoleType.USER);
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
--- src/main/java/com/khj/blog/service/UserService.java
+++ src/main/java/com/khj/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.khj.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.khj.blog.model.RoleType;
 import com.khj.blog.model.User;
 import com.khj.blog.repository.UserRepository;
 
@@ -13,8 +15,15 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+
 	@Transactional
 	public void 회원가입(User user) {
+		String rawPassword = user.getPassword();
+		String encPassword = encoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
 
