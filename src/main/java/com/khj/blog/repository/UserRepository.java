acc0bfcfbc7b7a9615369d301224fc0eab93b025
iamkhj18 <iamkhj18@naver.com>
2021-05-10 16:25:25 +0900

    스프링 시큐리티 로그인

--- src/main/java/com/khj/blog/repository/UserRepository.java
+++ src/main/java/com/khj/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.khj.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.khj.blog.model.User;
@@ -8,7 +10,7 @@ import com.khj.blog.model.User;
 // 자동으로 bean등록이 된다.
 // @Repository 생략이 가능하다.
 public interface UserRepository extends JpaRepository<User, Integer> {
-	
+	Optional<User> findByUsername(String username);
 }
 
 //JPA Naming 쿼리
